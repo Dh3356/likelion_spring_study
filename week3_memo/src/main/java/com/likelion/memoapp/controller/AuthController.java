a4b44befffebea67cb22af149a4254adb75db087
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-15 20:38:23 +0900

    feat: ResponseDto를 사용하도록 수정 - #5
    - SecurityUtil을 테스트하는 함수 추가

--- week3_memo/src/main/java/com/likelion/memoapp/controller/AuthController.java
+++ week3_memo/src/main/java/com/likelion/memoapp/controller/AuthController.java
@@ -1,15 +1,22 @@
 package com.likelion.memoapp.controller;
 
-import com.likelion.memoapp.model.dto.auth.AuthLoginRequestDTO;
-import com.likelion.memoapp.model.dto.auth.AuthSignupRequestDTO;
+import com.likelion.memoapp.dto.auth.AuthLoginRequestDto;
+import com.likelion.memoapp.dto.auth.AuthSignupRequestDto;
+import com.likelion.memoapp.dto.response.ResponseDto;
 import com.likelion.memoapp.service.AuthService;
+import com.likelion.memoapp.util.SecurityUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("auth")
 public class AuthController {
@@ -21,17 +28,24 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public void login(@RequestBody @Valid AuthLoginRequestDTO authLoginRequestDTO) {
-        this.authService.login(authLoginRequestDTO);
+    public ResponseEntity<ResponseDto<Void>> login(@RequestBody @Valid AuthLoginRequestDto authLoginRequestDTO,
+                                                   HttpServletResponse response) {
+        return this.authService.login(authLoginRequestDTO, response);
     }
 
     @PostMapping("/logout")
-    public void logout() {
-        this.authService.logout();
+    public ResponseEntity<ResponseDto<Void>> logout(HttpServletResponse response) {
+        return this.authService.logout(response);
     }
 
     @PostMapping("/signup")
-    public void signup(@RequestBody @Valid AuthSignupRequestDTO authSignupRequestDTO) {
-        this.authService.signup(authSignupRequestDTO);
+    public ResponseEntity<ResponseDto<Void>> signup(@RequestBody @Valid AuthSignupRequestDto authSignupRequestDTO) {
+        return this.authService.signup(authSignupRequestDTO);
+    }
+
+    //UserId를 받아올 수 있다. (로그인한 사용자의 정보를 받아올 수 있다.)
+    @GetMapping("/test")
+    public String test() {
+        return SecurityUtil.getCurrentUserId();
     }
 }
