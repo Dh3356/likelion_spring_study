b0365226a79ecb9b852dcb71265070e4248810a4
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-15 20:34:28 +0900

    feat: 로그인 시 User를 검증, UserDetails를 반환하는 클래스 구현 - #5
    - User가 존재한다면 UserDetails를 만들어 반환
    - 로그인 시 사용 예정

---
+++ week3_memo/src/main/java/com/likelion/memoapp/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.likelion.memoapp.service;
+
+import com.likelion.memoapp.model.User;
+import com.likelion.memoapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        return userRepository.findById(userId)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
+    }
+
+    // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 리턴
+    private UserDetails createUserDetails(User user) {
+        return User.builder()
+                .id(user.getId())
+                .password(passwordEncoder.encode(user.getPassword()))
+                .roles(user.getRoles())
+                .build();
+    }
+}
