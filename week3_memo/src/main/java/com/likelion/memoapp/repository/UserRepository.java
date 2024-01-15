f4cafcaad4da550f4ad27a7afa807b6e25602112
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-15 20:24:00 +0900

    refactor: User Entity 변경에 따른 리팩터링 - #5
    - 더 이상 email을 사용하지 않으므로 함수 삭제

--- week3_memo/src/main/java/com/likelion/memoapp/repository/UserRepository.java
+++ week3_memo/src/main/java/com/likelion/memoapp/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package com.likelion.memoapp.repository;
 
 import com.likelion.memoapp.model.User;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
-    boolean existsByEmail(String email);
+public interface UserRepository extends JpaRepository<User, String> {
 }
