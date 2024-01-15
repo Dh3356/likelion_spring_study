df1b28cb59a3a0cc8c495ba505332c0d15ad530d
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-15 20:30:21 +0900

    feat: Spring Security 설정을 위한 Config 클래스 구현 - #5
    - 로그인, 회원가입을 제외한 모든 요청에 Spring Security 적용
    - 위 경로를 제외한 모든 요청은 CorsFilter와 JwtFilter를 통과해야 함

---
+++ week3_memo/src/main/java/com/likelion/memoapp/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.likelion.memoapp.config;
+
+import com.likelion.memoapp.filter.JwtFilter;
+import com.likelion.memoapp.util.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CorsConfig corsConfig;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/signup", "/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilter(corsConfig.corsFilter())
+                .addFilterBefore(new JwtFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
