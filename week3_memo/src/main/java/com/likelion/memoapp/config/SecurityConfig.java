7448506e35cb62886587309981de02c0f83d274f
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-17 01:48:17 +0900

    feat: 로그아웃을 Spring Security가 수행하도록 변경 - #5
    - Cookie 삭제
    - Http 세션 무효화
    - 응답 데이터 설정

--- week3_memo/src/main/java/com/likelion/memoapp/config/SecurityConfig.java
+++ week3_memo/src/main/java/com/likelion/memoapp/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.likelion.memoapp.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.likelion.memoapp.dto.response.ResponseDto;
 import com.likelion.memoapp.filter.JwtFilter;
 import com.likelion.memoapp.util.JwtTokenProvider;
+import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,6 +29,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/signup", "/auth/login").permitAll()
                         .anyRequest().authenticated()
@@ -35,6 +40,28 @@ public class SecurityConfig {
                 .addFilter(corsConfig.corsFilter())
                 .addFilterBefore(new JwtFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class)
+                .logout(logout -> {
+                    logout
+                            .logoutUrl("/auth/logout") // 로그아웃 URL 지정
+                            .logoutSuccessHandler((request, response, authentication) -> {
+                                Cookie cookie = new Cookie("accessToken", null);
+                                cookie.setMaxAge(0);// 쿠키 만료
+                                cookie.setHttpOnly(true);
+                                cookie.setPath("/");
+                                response.addCookie(cookie);
+
+                                // 로그아웃 성공 시 200 OK 리턴
+                                String jsonResponse = new ObjectMapper().writeValueAsString(
+                                        ResponseDto.res(HttpStatus.OK, "로그아웃 되었습니다."));
+
+                                // HTTP 상태 코드 200 OK, JSON 형식 리턴
+                                response.setStatus(HttpStatus.OK.value());
+                                response.setContentType("application/json;charset=UTF-8");//응답 데이터 타입 지정
+                                response.getWriter().write(jsonResponse);//응답 데이터 출력
+                                response.getWriter().flush();//즉시 응답(더 빠름)
+                            })
+                            .invalidateHttpSession(true); // HTTP 세션 무효화
+                })
                 .build();
     }
 
