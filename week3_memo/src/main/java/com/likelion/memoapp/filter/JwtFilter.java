17fef7b1553140f23a9421d0cc5dd9a245e462d6
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-17 07:54:58 +0900

    comment: 주석 추가, import 최적화 - #5

--- week3_memo/src/main/java/com/likelion/memoapp/filter/JwtFilter.java
+++ week3_memo/src/main/java/com/likelion/memoapp/filter/JwtFilter.java
@@ -9,12 +9,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Slf4j
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
@@ -24,8 +22,9 @@ public class JwtFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         String token = resolveTokenFromCookie(request);
 
+        //토큰이 유효할 경우
         if (token != null && jwtTokenProvider.validateToken(token)) {
-            // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
+            //토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
