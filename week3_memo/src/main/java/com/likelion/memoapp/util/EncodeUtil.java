66ff886076cf416b88c637a5c62052971a452f3b
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-16 23:46:49 +0900

    feat: Cookie에 Jwt Token을 넣음에 따른 인코딩 문제를 해결하기 위한 EncodeUtil 구현 - #5
    - UTF-8로 인코딩, 디코딩
    - FrontEnd에서 Jwt 값을 파싱해야 하는 경우 디코딩 문제가 발생할 가능성이 있음
    - 하지만 Cookie에 설정하므로, FrontEnd에서는 다룰 일이 없음

---
+++ week3_memo/src/main/java/com/likelion/memoapp/util/EncodeUtil.java
@@ -0,0 +1,16 @@
+package com.likelion.memoapp.util;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class EncodeUtil {
+    public static String encodeJwtBearerToken(String token) {
+        return URLEncoder.encode("Bearer" + token,
+                StandardCharsets.UTF_8).replaceAll("\\+", "%20");
+    }
+
+    public static String decodeJwtBearerToken(String token) {
+        return URLDecoder.decode(token, StandardCharsets.UTF_8).split(" ")[1];
+    }
+}
